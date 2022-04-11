b24fbcfd225bf01b4ba56da1e254eb8839f7cea0
Estarlin21 <94544415+Estarlin21@users.noreply.github.com>
2022-04-11 13:52:30 +0200

    agregando mas script al desing-2

--- desing-2/js/script.js
+++ desing-2/js/script.js
@@ -1,16 +1,107 @@
-console.log('js is conected');
 let next = document.querySelectorAll('.next');
+let back = document.querySelectorAll('.back');
 let item = document.querySelectorAll('.item-2 .content-show-type-1');
 
 
-console.log(next);
+function nextPage(next, classRemove){
+    let items = document.querySelectorAll('.' + next + ' .child');
+    let page = document.querySelectorAll('.' + next + ' .page');
+    let pageCurrent = document.querySelectorAll('.' + next + ' .page-current');
+    pag = page[0];
 
-function nextPage(){
-    console.log('nextPage');
+    for (let index = items.length; index > 0; index--) {
+
+        const item = items[index -1 ];
+        
+        if(!findClass(classRemove, item.classList)){
+            pageCurrent.forEach(current => {
+                updateClass('current-page-green', current, false);
+            });
+            updateClass(classRemove, item, true);
+            
+            updateClass(classRemove, item, true);
+            
+            if (index == items.length ) { 
+                updateClass('current-page-green', pageCurrent[0], true);
+                pag.textContent = 1;
+                updateClass(classRemove, items[0], false);
+                return null;
+            }else{
+                updateClass('current-page-green', pageCurrent[index], true);
+                pag.textContent = index + 1;
+                updateClass(classRemove, items[index], false);
+            }
+        }
+    }
 }
 
-next.onclick = function(){
-    console.log('click me');
-};
+function backPage(next, classRemove){
+    let items = document.querySelectorAll('.' + next + ' .child');
+    let page = document.querySelectorAll('.' + next + ' .page');
+    let pageCurrent = document.querySelectorAll('.' + next + ' .page-current');
+    pag = page[0];
+    
+    for (let index = 0; index < items.length; index++) {
+        
+        
+        const item = items[index];
+        
+        if(!findClass(classRemove, item.classList)){
+            pageCurrent.forEach(current => {
+                updateClass('current-page-green', current, false);
+            });
+            updateClass(classRemove, item, true);
+            
+            if (index == 0 ) {
+                pag.textContent = items.length;
+                updateClass('current-page-green', pageCurrent[items.length - 1], true);
+                updateClass(classRemove, items[items.length -1], false);
+                return null;
+            }else{
+                updateClass('current-page-green', pageCurrent[index - 1], true);
+                pag.textContent = index;
+                updateClass(classRemove, items[index - 1], false);
+            }
+        }
+    }
+}
 
-next.addEventListener('click', nextPage);
+function updateClass(classes, item, add){
+    if(Array.isArray(classes)){
+        classes.forEach(clas => {
+            if(add){
+                item.classList.add(clas);
+            }else{
+                item.classList.remove(clas);
+            }
+        });
+    }else{
+        if(add){
+            item.classList.add(classes);
+        }else{
+            item.classList.remove(classes);
+        }
+    }
+}
+
+function findClass(singleClass, classes){
+    let value = false;
+    classes.forEach(clas => {
+        if(clas == singleClass){
+            value = true;
+        }
+    });
+    return value;
+}
+
+
+
+
+next.forEach(nextE => {
+    nextE.onclick = function() {nextPage(nextE.name, 'content-dnone')};
+
+});
+
+back.forEach(nextE => {
+    nextE.onclick = function() {backPage(nextE.name, 'content-dnone')};
+});
