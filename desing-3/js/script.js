0ed9d0eff31f515653401d44562cc3ebbc194f35
Estarlin21 <94544415+Estarlin21@users.noreply.github.com>
2022-05-23 21:23:56 +0200

    completando el diseno 3

--- desing-3/js/script.js
+++ desing-3/js/script.js
@@ -9,6 +9,14 @@ function dom(){
 
 function myProgram(){
     var lateral_menu = document.querySelector('.lateral-menu-1')
+    var options = document.querySelectorAll('.option-3 p');
+    
+    options.forEach(option => {
+        option.addEventListener('click', function(){
+            searchHtml(option, options, 'active-1')
+        })
+    });
+
     lateral_menu.addEventListener('mouseover', function(){
         lateralChange(true, lateral_menu, 'hidden-p')
     })
@@ -29,3 +37,18 @@ function lateralChange(action, item, classM){
         }
     }
 }
+
+function searchHtml(option, options, classM){
+    options.forEach(optio => {
+        lateralChange(true, optio, 'active-1')
+        let item = document.querySelector('.' + optio.innerHTML)
+        lateralChange(false, item, 'dnone')
+    });
+
+    
+    item = document.querySelector('.' + option.innerHTML)
+    lateralChange(true, item, 'dnone')
+    lateralChange(false, option, 'active-1')
+}
+
+
