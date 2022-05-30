1b8b858c29331a33e6af51bc090334c240104cc4
Estarlin21 <94544415+Estarlin21@users.noreply.github.com>
2022-05-30 21:54:37 +0200

    pausando el desing 4

--- desing-4/js/script.js
+++ desing-4/js/script.js
@@ -16,7 +16,6 @@ function time(){
     var hours1 = document.getElementById('hours1')
     var minutes1 = document.getElementById('minutes1')
     var seconds1 = document.getElementById('seconds1')
-    var timer = document.getElementById('timer')
 
     var timers = new Date(4044, 5, 25, 12, 30, 01, 00)
     var time = new Date()
@@ -39,8 +38,8 @@ function time(){
         seconds.innerHTML = time.getSeconds().toString()
         if(seconds.innerHTML == 0 || seconds.innerHTML == 59 ){
             minutes.innerHTML = time.getMinutes()
-            if(minutes.innerHTML == 0){
-                hours.innerHTML = time.getHours()
+            if(minutes.innerHTML == 0 || seconds.innerHTML == 59){
+                hours.innerHTML = time.getHours(minutes)
             }
         }
     }
@@ -52,36 +51,110 @@ function main(){
     time()
 
     var ball = document.querySelector('.circle')
+    var ball2 = document.querySelector('.ball')
     var addP = true;
     var addU = true;
+    var addBL = true
+    var addBU = true
+
+    setInterval(function() {
+        moveB(ball2, 15);
+    }, 100)
+
     setInterval(function() {
         move(ball);
-    }, 1)
+    }, 50)
+
 
     function move(object){
-
         if(addP){
-            object.style.left = (parseFloat(object.style.left) + .1) + '%'
-            if(parseFloat(object.style.left) > 80){
+            object.style.left = (parseFloat(object.style.left) + .5) + 'vw'
+            if(parseFloat(object.style.left) > 85){
                 addP = false
             }
         }else{
-            object.style.left = (parseFloat(object.style.left) - .1) + '%'
+            object.style.left = (parseFloat(object.style.left) - .5) + 'vw'
             if(parseFloat(object.style.left) < 0){
                 addP = true
             }
         }
 
         if(addU){
-            object.style.top = (parseFloat(object.style.top) + .2) + '%'
-            if(parseFloat(object.style.top) > 80){
+            object.style.top = (parseFloat(object.style.top) + 1) + 'vh'
+            if(parseFloat(object.style.top) > 85){
                 addU = false
             }
         }else{
-            object.style.top = (parseFloat(object.style.top) - .2) + '%'
+            object.style.top = (parseFloat(object.style.top) - 1) + 'vh'
             if(parseFloat(object.style.top) < 0){
                 addU = true
             }
         }
+    }
+
+    function moveB(object, max){
+
+        if(addBU){
+            if(parseFloat(object.style.left) > ( max/ 2)){
+                if(parseFloat(object.style.top) > max - parseFloat(object.style.left) + (max / 2)){
+                    addBU = false
+                }else{
+                    object.style.top = parseFloat(object.style.top) + 1 + 'rem'
+                }
+            }else{
+                if(parseFloat(object.style.top) >  parseFloat(object.style.left) + (max / 2)){
+                    addBU = false
+                }else{
+                    object.style.top = parseFloat(object.style.top) + 1 + 'rem'
+                }
+            }
+
+        }else{
+            if(parseFloat(object.style.left) > (max / 2)){
+                if(parseFloat(object.style.top) < parseFloat(object.style.left) - (max / 2)){
+                    addBU = true
+                }else{
+                    object.style.top = parseFloat(object.style.top) - 1.1 + 'rem'
+                }
+            }else{
+                if(parseFloat(object.style.top) < max - parseFloat(object.style.top) - (max / 2)){
+                    addBU = true
+                }else{
+                    object.style.top = parseFloat(object.style.top) - 1.1 + 'rem'
+                }
+            }
+        }
+
+        if(addBL){
+            if(parseFloat(object.style.top) > (max / 2)){
+                if(parseFloat(object.style.left) > (max - parseFloat(object.style.top) + (max / 2))){
+                    addBL = false
+                }else{
+                    object.style.left = parseFloat(object.style.left) + 1.1 + 'rem'
+                }
+            }else{
+                if(parseFloat(object.style.left) > parseFloat(object.style.top) + (max / 2)){
+                    addBL = false
+                }else{
+                    object.style.left = parseFloat(object.style.left) + 1.1 + 'rem'
+                }
+            }
+        }else{
+            if(parseFloat(object.style.top) > 6){
+                if(parseFloat(object.style.left) <  parseFloat(object.style.top) - (max / 2)){
+                    addBL = true
+                }else{
+                    object.style.left = parseFloat(object.style.left) - 1 + 'rem'
+                }
+            }else{
+                if(parseFloat(object.style.left) < (max /2) - parseFloat(object.style.top)){
+                    addBL = true
+                }else{
+                    object.style.left = parseFloat(object.style.left) - 1 + 'rem'
+                }
+            }
+            
+        }
+
     }
 }
