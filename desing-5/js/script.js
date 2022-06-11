79b3ae5d79eb4e547aed0a7dbcafed07ac6da45c
Estarlin21 <94544415+Estarlin21@users.noreply.github.com>
2022-06-11 13:16:03 +0200

    desing-5 agregando modificas al reprodutor de videos

--- desing-5/js/script.js
+++ desing-5/js/script.js
@@ -8,39 +8,20 @@ function pageReady(){
     }
 }
 
+
+var currentVideo
+var videoStatus1
 function main(){
 
 
     // video player
     var sections = document.querySelectorAll('.sections')
-    var playVideo
     sections.forEach((section) => {
-        section.addEventListener('click', () => { 
-            changeWidth(section, sections)
-            changeClass(section, sections, 'play', 'paused')
-            if(section.firstElementChild != null){
-                if(section.firstElementChild.paused){
-                    if(playVideo != undefined){
-                        playVideo.firstElementChild.pause()
-                        changeClass(playVideo.lastElementChild.firstElementChild.firstElementChild, null, 'PlayB', 'PauseB')
-                    }
-                    playVideo = section
-                    screen = playVideo.lastElementChild.firstElementChild.lastElementChild
-                    section.requestFullscreen()
-                    section.firstElementChild.addEventListener('ended', () => {
-                        changeClass(playVideo.lastElementChild.firstElementChild.firstElementChild, null, 'PlayB', 'PauseB')
-                    })
-                    changeClass(playVideo.lastElementChild.firstElementChild.firstElementChild, null, 'PauseB', 'PlayB')
-                    section.firstElementChild.play()
-                }else{
-                    changeClass(playVideo.lastElementChild.firstElementChild.firstElementChild, null, 'PlayB', 'PauseB')
-                    section.firstElementChild.pause()
-                }
-            }else if(playVideo != undefined){
-                playVideo.firstElementChild.pause()
-                changeClass(playVideo.lastElementChild.firstElementChild.firstElementChild, null, 'PlayB', 'PauseB')
-            }
-        })
+        if(section.childElementCount > 0){
+            section.addEventListener('click', () => {
+                currentVideo = videoPlayer(section, sections)
+            })
+        }
     })
 
     // menu drop
@@ -105,5 +86,109 @@ function changeWidth(item, items){
 
         item.style.width = '88vw'
         item.style.height = '49.5vw'
+    }
+}
+
+function removeEventVideo(){
+    currentVideo.play.removeEventListener('click', pause)
+    currentVideo.fullScreen.removeEventListener('click', fullScreen)
+}
+
+// video player
+function videoPlayer(item, items){
+    
+    changeWidth(item, items)
+    changeClass(item, items, 'play', 'paused')
+    var newCurrent = new video(item)
+
+    if(currentVideo == undefined){
+        currentVideo = newCurrent
+        pause()
+        addEventVideo()
+    }
+    
+    if(currentVideo.audio != newCurrent.audio){
+        removeEventVideo()
+        
+        currentVideo.video.pause()
+        currentVideo = newCurrent
+        pause()
+        addEventVideo()
+    }
+    
+    function addEventVideo(){
+        currentVideo.play.addEventListener('click', pause)
+        currentVideo.video.addEventListener('ended', pause)
+        currentVideo.fullScreen.addEventListener('click', fullScreen)
+        videoStatus1 = setInterval(videoStatus, 100)
+    }
+
+    function removeEventVideo(){
+        currentVideo.play.removeEventListener('click', pause)
+        currentVideo.video.removeEventListener('ended', pause)
+        currentVideo.fullScreen.removeEventListener('click', fullScreen)
+        clearInterval(videoStatus1)
+    }
+
+    return currentVideo
+}
+    
+// PlayPausa
+function pause(){
+    videoStatus1 = setTimeout(videoStatus, 10)
+    var items = document.querySelectorAll('.sections')
+    
+    if(currentVideo.video.paused){
+        currentVideo.video.play()
+        currentVideo.play.classList.remove('PlayB')
+    }else{
+        currentVideo.play.classList.add('PlayB')
+        currentVideo.video.pause()
+    }
+}
+
+function videoStatus(){
+    let status = currentVideo.video.currentTime
+    let duration = currentVideo.video.duration
+    let playStatus = status * 100 / duration + '%'
+    currentVideo.status.firstElementChild.style.width = playStatus
+}
+
+function fullScreen(){
+    if(currentVideo.video.requestFullscreen){
+        currentVideo.video.requestFullscreen()
+    }else{
+        document.exitFullscreen()
+    }
+}
+
+
+
+
+class video{
+    constructor(item){
+
+        this.fullScreen = this.search(item, 'fullScreen')
+        this.audio = this.search(item, 'muted')
+        this.status = this.search(item, 'videoStatus')
+        this.play = this.search(item, 'PauseB')
+        this.item = item
+        this.video = item.firstElementChild
+    }
+
+    search(item, classN){
+
+        var videoMenus = document.querySelectorAll('.videoMenu')
+        videoMenus.forEach(videoMenu => {
+            if(videoMenu.parentElement.parentElement == item){
+                for (let elementN = 0; elementN < videoMenu.children.length; elementN++) {
+                    if(videoMenu.children[elementN].classList.contains(classN)){
+                        this.returnItem = videoMenu.children[elementN]
+                    }
+                }
+            }
+        })
+
+        return this.returnItem
     }
 }
